3464b3987607ff5a50612d9d6f3bcde63481ebfd
MinJun916 <ed125248@gmail.com>
2025-08-28 18:36:51 +0900

    feat: create Habit get API (not tested yet)

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PrismaClient } from "@prisma/client";
 import swaggerUi from "swagger-ui-express";
 import { specs, swaggerUiOptions } from "./src/swaggerOptions.js";
 import morgan from "morgan";
+import bcrypt from "bcryptjs";
 
 dotenv.config();
 
@@ -22,6 +23,42 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, swaggerUiOptions));
 app.use(morgan("combined"));
 
 // ... API 코드를 작성해 주세요.
+
+// 오늘의 습관 API
+app.get("/studies/:studyId/habits", async (req, res) => {
+  const { studyId } = req.params;
+  const password =
+    typeof req.query.password === "string" ? req.query.password : undefined;
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "비밀번호를 입력해주세요" });
+  }
+
+  const study = await prisma.study.findUnique({
+    where: { id: studyId },
+    select: { id: true, password: true },
+  });
+
+  if (!study) {
+    return res
+      .status(404)
+      .json({ success: false, message: "스터디를 찾을 수 없습니다" });
+  }
+
+  const okStudy = await bcrypt.compare(password, study.password);
+  if (!okStudy) {
+    return res.status(401).json({ success: false, message: "비빌번호 불일치" });
+  }
+
+  const habits = await prisma.habit.findMany({
+    where: { studyId },
+    select: { id: true, name: true },
+  });
+
+  return res.json({ success: true, habits });
+});
 
 // 404 핸들러
 app.use("*", (req, res) => {
