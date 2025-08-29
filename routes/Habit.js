0ff2641c80af455527608ecc9626555a43ac3e13
MinJun916 <ed125248@gmail.com>
2025-08-29 16:28:23 +0900

    chore: temporarily remove bcrypt code and add http file for testing

--- routes/Habit.js
+++ routes/Habit.js
@@ -6,6 +6,12 @@ const router = express.Router();
 const prisma = new PrismaClient();
 
 // 오늘의 습관 API
+// 오늘의 습관 조회
+router.get("/", async (req, res) => {
+  const studies = await prisma.study.findMany();
+  return res.json({ success: true, studies });
+});
+
 router.get("/:studyId/habits", async (req, res) => {
   const { studyId } = req.params;
   const password =
@@ -28,10 +34,17 @@ router.get("/:studyId/habits", async (req, res) => {
       .json({ success: false, message: "스터디를 찾을 수 없습니다" });
   }
 
-  const okStudy = await bcrypt.compare(password, study.password);
+  //임시 테스트 코드 (암호화 적용 X)
+  const okStudy = Boolean(password === study.password);
   if (!okStudy) {
     return res.status(401).json({ success: false, message: "비빌번호 불일치" });
   }
+
+  // bcrypt 코드 (테스트 예정)
+  // const okStudy = await bcrypt.compare(password, study.password);
+  // if (!okStudy) {
+  //   return res.status(401).json({ success: false, message: "비빌번호 불일치" });
+  // }
 
   const habits = await prisma.habit.findMany({
     where: { studyId },
@@ -40,5 +53,7 @@ router.get("/:studyId/habits", async (req, res) => {
 
   return res.json({ success: true, habits });
 });
+
+// 오늘의 습관 생성
 
 export default router;
