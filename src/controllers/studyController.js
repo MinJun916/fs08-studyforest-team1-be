7d59b1ac9c88a40898f855e2f684ef96e85b305b
MinJun916 <ed125248@gmail.com>
2025-09-02 18:47:36 +0900

    refactor: change study and emoji routers to use res.json

--- src/controllers/studyController.js
+++ src/controllers/studyController.js
@@ -8,11 +8,17 @@ import {
 
 export const getStudies = async (req, res, next) => {
   try {
-  const offset = req.query?.offset !== undefined ? parseInt(req.query.offset, 10) : undefined;
-  const limit = req.query?.limit !== undefined ? parseInt(req.query.limit, 10) : undefined;
-  const order = req.query?.order;
-  const studies = await listStudies({ offset, limit, order });
-    res.send(studies);
+    const offset =
+      req.query?.offset !== undefined
+        ? parseInt(req.query.offset, 10)
+        : undefined;
+    const limit =
+      req.query?.limit !== undefined
+        ? parseInt(req.query.limit, 10)
+        : undefined;
+    const order = req.query?.order;
+    const studies = await listStudies({ offset, limit, order });
+    res.status(200).json({ success: true, data: studies });
   } catch (err) {
     next(err);
   }
@@ -20,9 +26,9 @@ export const getStudies = async (req, res, next) => {
 
 export const getStudy = async (req, res, next) => {
   try {
-  const id = req.params?.id;
-  const study = await getStudyById(id);
-    res.send(study);
+    const id = req.params?.id;
+    const study = await getStudyById(id);
+    res.status(200).json({ success: true, data: study });
   } catch (err) {
     next(err);
   }
@@ -30,8 +36,8 @@ export const getStudy = async (req, res, next) => {
 
 export const postStudy = async (req, res, next) => {
   try {
-  const created = await createStudy(req.body ?? {});
-    res.status(201).send(created);
+    const created = await createStudy(req.body ?? {});
+    res.status(201).json({ success: true, data: created });
   } catch (err) {
     next(err);
   }
@@ -39,9 +45,9 @@ export const postStudy = async (req, res, next) => {
 
 export const patchStudy = async (req, res, next) => {
   try {
-  const id = req.params?.id;
-  const updated = await updateStudy(id, req.body ?? {});
-    res.send(updated);
+    const id = req.params?.id;
+    const updated = await updateStudy(id, req.body ?? {});
+    res.status(200).json({ success: true, data: updated });
   } catch (err) {
     next(err);
   }
@@ -49,8 +55,8 @@ export const patchStudy = async (req, res, next) => {
 
 export const deleteStudyCtrl = async (req, res, next) => {
   try {
-  const id = req.params?.id;
-  await deleteStudy(id);
+    const id = req.params?.id;
+    await deleteStudy(id);
     res.sendStatus(204);
   } catch (err) {
     next(err);
